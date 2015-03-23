8d3c5c0d40adb1a7488bde700a6d0f9d3064c277
kanovas <kano.vas.2.0@gmail.com>
2015-03-23 19:19:55 +0300

    lenwords

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 run: 
 	cd lib && make
 	cd cat && make
+	cd revwords && make
+	cd delwords && make
--- lib/helpers.c
+++ lib/helpers.c
@@ -14,13 +14,30 @@ ssize_t read_(int fd, void *buf, size_t count) {
 }
 
 ssize_t write_(int fd, const void* buf, size_t count) {
-    size_t curr = 0;
-    for( ; ;) {
-        ssize_t written = write(fd, buf + curr, count - curr);
-        if (written == -1)
-            return -1;
-        curr += written;
-        if (curr >= count)
-            return curr;
-    }
+  size_t curr = 0;
+  for( ; ;) {
+    ssize_t written = write(fd, buf + curr, count - curr);
+    if (written == -1)
+      return -1;
+    curr += written;
+    if (curr >= count)
+      return curr;
+  }
 }
+
+
+ssize_t read_until(int fd, void * buf, size_t count, char delimiter) {
+    int curr, n;
+    for (curr = 0; curr < count; curr++) {
+        n = read(fd, buf + curr, 1);
+        if (n == -1)
+            return -1;
+        if (n == 0)
+            return curr;
+        if ((((char*)buf)[curr]) == delimiter)
+            return curr + 1;
+    }
+
+    return curr;
+}
+
